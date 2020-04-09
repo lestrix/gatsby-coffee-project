5accbe8bed05bef31b86cd27e79f7bb7c6ebe638
lestrix <lestrix@gmx.de>
2020-04-09 16:20:10 +0200

    Navbar added as well as basic CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new toiletshop site.</p>
     <p>Now go build something great.</p>
     <p>
       TEST <FaGulp />
