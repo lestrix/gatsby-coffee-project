9ec934530b64bedd4e7b73304303415f5956eb35
lestrix <lestrix@gmx.de>
2020-04-16 09:01:16 +0200

    added menu

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -10,9 +10,9 @@ export default function Info() {
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
               Herzlichen Willkommen im Online Shop von Mondwolke! <br />
-              Durch die Umstände der Covid-19 Krise, sind Desiree und ich auf
-              die gloreiche Idee gekommen unseren eigenen Webshop zu basteln und
-              im Zuge dessen freuen wir uns sehr Euch unsere Mondwolke Website
+              Durch die Umstände der Covid-19 Krise, sind wir auf die gloreiche
+              Idee gekommen unseren eigenen Webshop zu basteln und im Zuge
+              dessen freuen wir uns sehr Euch unsere Mondwolke Website
               vorstellen zu können
             </p>
             <Link to="/about/">
