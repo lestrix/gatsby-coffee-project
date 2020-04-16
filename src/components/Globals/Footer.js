6a932e67ce8c964af9ba3cfd6f425735ed2ec6ca
lestrix <lestrix@gmx.de>
2020-04-16 08:22:37 +0200

    connected contentful

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="row">
           <div className="col-10 mx-auto col-md-6 text-grey text-center text-capitalize">
             <h3>
-              all right reserved &copy;{new Date().getFullYear().toString()}
+              all rights reserved &copy;{new Date().getFullYear().toString()}
             </h3>
           </div>
         </div>
